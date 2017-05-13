a3e0611a0f37e5eac23e8f41105ec0cd816c1aa9
Anton <goncharenko@vismo.com.ua>
2017-05-13 22:22:59 +0300

    59 Added hotel services to configuration page

--- src/app/admin-panel/dashboard/configuration-page/configuration-page.component.ts
+++ src/app/admin-panel/dashboard/configuration-page/configuration-page.component.ts
@@ -1,52 +1,68 @@
 import {AfterViewInit, Component, OnInit} from '@angular/core';
 import {FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
 import {ToursService} from "../../../shared/services/tours.service";
-declare let $:any;
+declare let $: any;
 
 @Component({
   selector: 'app-configuration-page',
   templateUrl: './configuration-page.component.html',
   styleUrls: ['./configuration-page.component.scss']
+
 })
-export class ConfigurationPageComponent implements OnInit, AfterViewInit{
-   countryControl;
-   cityControl = {};
-   countriesList;
-   newAddedCountryKey;
+export class ConfigurationPageComponent implements OnInit {
+  countryControl;
+  servicesControl;
+  cityControl = {};
 
-   constructor(public fb:FormBuilder,
-                public toursService:ToursService) { }
+  countriesList = [];
+  servicesList = [];
 
-   ngOnInit() {
-     this.getCountriesList();
-   }
+  newAddedCountryKey;
 
-   ngAfterViewInit() {}
+  constructor(public fb: FormBuilder,
+              public toursService: ToursService) {}
 
-   onSaveCountry() {
-     this.toursService.list('configurations/countries').push({country:this.countryControl}).then((response)=>{
-       this.newAddedCountryKey = response[response.path.o.length-1];
-     });
-     this.countryControl = null;
-     this.newAddedCountryKey = null;
-   }
+  ngOnInit() {
+    this.getCountriesList();
+    this.getServicesList();
+  }
 
-   onSaveCity(countryKey,cityControlKey) {
-     let key = this.newAddedCountryKey ? this.newAddedCountryKey : countryKey;
-     this.toursService.list(`/configurations/countries/${key}/cities`).push(this.cityControl[cityControlKey]).then((responseCity)=>{
-       this.getCountriesList();
-       this.cityControl[cityControlKey] = null;
-     });
-   }
+  onSaveCountry() {
+    this.toursService.list('configurations/countries').push({country: this.countryControl}).then((response) => {
+      this.newAddedCountryKey = response[response.path.o.length - 1];
+    });
+    this.countryControl = null;
+    this.newAddedCountryKey = null;
+  }
 
-   getCountriesList() {
-     this.toursService.list('configurations/countries').subscribe(countriesWithCities => {
-       this.countriesList = countriesWithCities;
-       $(document).ready(function(){
-           $('.collapsible').collapsible();
-       });
+  onSaveService() {
+    this.toursService.list('configurations/hotelServices').push({title: this.servicesControl}).then(() => {
+      this.servicesControl = null;
+    })
+  }
 
-     })
-   }
+  getServicesList() {
+    this.toursService.list('configurations/hotelServices').subscribe(servicesList => {
+      this.servicesList = servicesList;
+    })
+  }
+
+  onSaveCity(countryKey, cityControlKey) {
+    let key = this.newAddedCountryKey ? this.newAddedCountryKey : countryKey;
+    this.toursService.list(`/configurations/countries/${key}/cities`).push(this.cityControl[cityControlKey]).then((responseCity) => {
+      this.getCountriesList();
+      this.cityControl[cityControlKey] = null;
+    });
+  }
+
+  getCountriesList() {
+    this.toursService.list('configurations/countries').subscribe(countriesWithCities => {
+      this.countriesList = countriesWithCities;
+      $(document).ready(function () {
+        $('.collapsible').collapsible();
+      });
+
+    })
+  }
 
 }

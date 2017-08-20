0597974781fa3cbbef816c62188acddda589aed5
Anton <goncharenko@vismo.com.ua>
2017-08-20 20:16:32 +0300

    114. Server config (test with static files)

--- demon-server/server.js
+++ demon-server/server.js
@@ -1,11 +1,11 @@
 var express = require("express");
 var app = express();
 app.use(express.static('dist'));
-app.all('/portal/*', (req, res) => {
-  res.status(200).sendfile('./dist/');
-});
+// app.all('/portal/*', (req, res) => {
+//   res.status(200).sendfile('./dist/');
+// });
 
-var port = '4300';
+var port = '80';
 app.listen(port, function() {
   console.log("Listening on " + port);
 });

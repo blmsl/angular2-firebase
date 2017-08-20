d5cac64048e636943ab2cc1722854f68b19e5f1c
Anton <goncharenko@vismo.com.ua>
2017-08-20 20:12:33 +0300

    113. Server config (Resolve Angualr routing on express server)

--- demon-server/server.js
+++ demon-server/server.js
@@ -1,23 +1,11 @@
 var express = require("express");
 var app = express();
-
-/* serves main page */
-app.get("/", function(req, res) {
-  res.sendfile('./dist/')
+app.use(express.static('dist'));
+app.all('/portal/*', (req, res) => {
+  res.status(200).sendfile('./dist/');
 });
 
-app.post("/user/add", function(req, res) {
-  /* some server side logic */
-  res.send("OK");
-});
-
-/* serves all the static files */
-app.get(/^(.+)$/, function(req, res) {
-  console.log('static file request : ' + req.params[0]);
-  res.sendfile('dist/' + req.params[0]);
-});
-
-var port = process.env.PORT || 80;
+var port = '4300';
 app.listen(port, function() {
   console.log("Listening on " + port);
 });

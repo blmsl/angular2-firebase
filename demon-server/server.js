f7ecc59767ba1fbe564c6b2183fc7ee4106998a5
Anton <goncharenko@vismo.com.ua>
2017-08-20 22:07:13 +0300

    115. Server config (Added gzip compression)

--- demon-server/server.js
+++ demon-server/server.js
@@ -1,11 +1,14 @@
 var express = require("express");
+var compression = require('compression');
 var app = express();
+app.use(compression());
 app.use(express.static('dist'));
-// app.all('/portal/*', (req, res) => {
-//   res.status(200).sendfile('./dist/');
-// });
 
-var port = '80';
+app.all('*', (req, res) => {
+  res.sendfile('dist/');
+});
+
+var port = '4300';
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
